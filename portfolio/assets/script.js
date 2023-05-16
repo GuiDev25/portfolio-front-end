de6aa2d099a8b35769eef2586619c4f37a6c87e0
Guilherme <p.galmeidafernandes@gmail.com>
2023-05-15 23:20:22 -0300

    Att nav menu e mensagem
    Refiz o nav bar já com a responsividade e comecei a primeira parte do body

---
+++ portfolio/assets/script.js
@@ -0,0 +1,8 @@
+function showmenu(){
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+    }else{
+        menuMobile.classList.add('open');
+    }
+}
