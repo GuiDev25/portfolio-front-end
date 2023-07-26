cbdff784baddcfe49ea1c49406370a4ea2ee34d1
Guilherme <p.galmeidafernandes@gmail.com>
2023-07-26 11:46:08 -0300

    Portfolio Desktop finish
    Adição do botão voltar ao topo e finalização da sessão 3

--- portfolio/assets/script.js
+++ portfolio/assets/script.js
@@ -6,3 +6,14 @@ function showmenu(){
         menuMobile.classList.add('open');
     }
 }
+window.addEventListener('scroll', function(){
+    let scroll = document.querySelector('.scrolltop');
+        scroll.classList.toggle('active', window.scrollY > 720);
+})
+
+function voltar(){
+    window.scrollTo({
+        top:0,
+        behavior:'smooth'
+    })
+}
